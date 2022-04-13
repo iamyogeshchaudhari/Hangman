55a469a64356b5085138eee177097df8a514442d
iamyogeshchaudhari <iamyogeshchaudhari@gmail.com>
2022-04-13 15:39:54 +0530

    Added letter checks (if exists in the word)

--- Hangman.py
+++ Hangman.py
@@ -10,5 +10,17 @@ selected_word = random.choice(word_list)
 #Creating as many blanks as the letters in the word.
 display_list = []
 for n in selected_word:
-    display_list.append["_"]
+    display_list.append("_")
 
+#Guess a word
+guessed_letter = input("Guess a letter: ").lower()
+
+#Check if the letter exists in the word and replace in list
+n = -1
+
+for x in selected_word:
+    n = n+1
+    if guessed_letter == x:
+        display_list[n] = guessed_letter
+
+print (display_list)
