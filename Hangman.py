f365e855ba65077328a4956a6e8aa304e650ebee
iamyogeshchaudhari <iamyogeshchaudhari@gmail.com>
2022-04-14 09:47:39 +0530

    Final commit.
    Everything working as expected.

--- Hangman.py
+++ Hangman.py
@@ -1,46 +1,49 @@
 import random
 import Hangman_art
+import Hangman_words
 
-print (Hangman_art.logo)
+#Printing hangman art logo.
+print (Hangman_art.logo) 
 
-#List of Words
-word_list = ["bull", "sheep", "cow"]
-
-#Selecting a word randomly
-selected_word = random.choice(word_list)
-
-#Creating as many blanks as the letters in the word.
-display_list = []
-for n in selected_word:
-    display_list.append("_")
-
-#Definitions
-n = -1
+#Definitions:
+word_list = Hangman_words.word_list #list of words.
+n = int(-1) #position
 user_lives = 6
 game_ended = False
 
+#Selecting a word randomly.
+selected_word = random.choice(word_list) 
+
+#Creating as many blanks as the letters in the word.
+display_list = []
+for o in selected_word:
+    display_list.append("_")
+
 #Creating a loop until the user guesses all words, or exhausts all lives.
 while game_ended == False:
+    guessed_letter = input("\nGuess a letter: ").lower()
 
-    guessed_letter = input("Guess a letter: ").lower()
-
-    # Check if the letter exists in the word and replace in list, or cost a life.
+    #Check if the letter exists in the word and replace in list, or cost a life. (in reverse order)
     if guessed_letter not in selected_word:
         user_lives = user_lives - 1
 
     for x in selected_word:
-        n = n+1
+        n = n + 1
         if guessed_letter == x:
             display_list[n] = guessed_letter
-    
-    n = -1
 
+    n = -1 #Resetting value of n to -1.
+
+    #Stopping the loop after all letters revealed, or all lives done.
     if "_" not in display_list or user_lives == 0:
         game_ended = True
     
+    #Printing List and ASCII art for lives after every turn.
     print (display_list)
+    print(Hangman_art.stages[user_lives]) 
 
+#Printing if the player won or lost.
 if "_" not in display_list:
     print("You won.")
-elif user_lives:
-    print("You lost. The word was" + selected_word)
+elif user_lives == 0:
+    print("You lost. The word was " + selected_word)
